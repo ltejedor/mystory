e67206497fa37cb4b4d191cb89f21338ae381760
Leandra Tejedor <lmtejedor@gmail.com>
2014-02-11 23:08:49 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystory::Application.config.secret_key_base = '679d5d7f9fd953201adfdb6ab14cde572e6af8978d3357c9a984702b10736dc19427202b6fe87fc324ca299e4be11b5726071d46eb7119db178307ff3c335546'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mystory::Application.config.session_store :cookie_store, key: '_mystory_session'
